26a57eb5e5c7d8c471767e5a09ac4ec5976e6301
Ana Viktoriv <anaviktoriv@gmail.com>
2020-02-17 20:28:31 +0100

    fini ex 4

---
+++ Ex-4/script.js
@@ -0,0 +1,26 @@
+//jshint esversion: 6
+let ageString;
+let ageInt;
+let jeunes = 0;
+let moyens = 0;
+let vieux = 0;
+
+
+
+
+
+do {
+  ageString = prompt("Entrer l'age entre 0 et 100");
+  ageInt = parseInt(ageString);
+  if (ageInt >= 0 && ageInt < 20) {
+    jeunes++;
+  } else if (ageInt >= 20 && ageInt <= 40) {
+    moyens++;
+  } else if (ageInt > 40) {
+    vieux++;
+  } else if (ageInt === 100) {
+    vieux++;
+  }
+} while (ageInt !== 100);
+
+alert("jeunes: " + jeunes + " moyens: " + moyens + " vieux: " + vieux);
